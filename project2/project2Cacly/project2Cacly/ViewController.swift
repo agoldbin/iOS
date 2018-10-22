bd93394be9faa224fcc16e28b98e1aa698ac0289
Aaron Goldbin <31373437+agoldbin@users.noreply.github.com>
2018-10-22 05:50:51 -0500

    cant do more than 2 numbers at a time. have to hit equal between each set of nums

--- project2/project2Cacly/project2Cacly/ViewController.swift
+++ project2/project2Cacly/project2Cacly/ViewController.swift
@@ -14,111 +14,92 @@ class ViewController: UIViewController {
     var symbol: Int = 0
     let toggle = "+/-"
     var currentString = ""
-    var performMath = false
-    var isFirstNumber = true
-    var isTypingNumber = false
-    
-//    struct MathStuff {
-//        var currentNumber: Double?
-//        var previousNumber: Double?
-//
-//        init?(currentNumber: Double?, previousNumber: Double?, isFirstNumber: Bool) {
-//            if (isFirstNumber) {
-//                // initalize all attributes
-//                self.previousNumber = currentNumber
-//                print("In init")
-//            } else {
-//                return nil
-//            }
-//        }
-//    }
+    var isTypingNumber: Bool?
+    var isFirstOperator: Bool?
 
-//    init?(currentNumber: Double?, previousNumber: Double?) {
-//
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        isTypingNumber = false
+        isFirstOperator = true
         totalLabel.text = "Hello world"
     }
     
-
+    @IBAction func clearOrEqual(_ sender: UIButton) {
+        let title = sender.tag
+        switch title {
+            case 11:
+            // clear
+                totalLabel.text = ""
+                isTypingNumber = true
+                isFirstOperator = true
+                firstNumber = 0
+                currentNumber = 0
+                break
+            case 16:
+            // =
+                performOperation()
+                return
+            case 18:
+            // +/-
+                currentNumber = toggleValue(currentNumber: currentNumber)
+                return
+            default:
+                totalLabel.text = ""
+                print("Err in clear or equal switch")
+                break
+        }
+    }
+    
+    
     /**
      * Determine what symbol the user pressed and perform appropriate actions
      */
     @IBAction func operatorTouched(_ sender: UIButton) {
         let title = sender.tag
-//        if (isFirstNumber) {
-//            previousNumber = Double(currentNumber)
-//            isFirstNumber = false
-//            print("First num entered: \(previousNumber)")
+//        print(totalLabel.text)
+//        if (isFirstOperator! == nil) {
+//            isFirstOperator = true
 //        }
-//        currentString = ""
-//        print("previous num: \(firstNumber)")
+//        if !isFirstOperator! {
+//            performOperation()
+//        }
         currentString = totalLabel.text!
-        print("current string: \(currentString)")
-        isTypingNumber = false
-        switch title {
-            case 11:
-            // clear
-                totalLabel.text = ""
-//                isFirstNumber = true
-                isTypingNumber = true
-                firstNumber = 0
-                currentNumber = 0
-                
-                //                currentString = ""
-//                totalLabel.text = ""
-                break
-    
-            // If btn pressed was a calculation btn
-            case 12 ... 17:
-//                var currentString = totalLabel.text
-//                isTypingNumber = false
-//                if isTypingNumber {
-                    firstNumber = Double(currentString)!
-                    print("Nested switch, 1st num: \(firstNumber)")
-//                }
-                switch title {
-                    case 12:
-                        // *
-                        totalLabel.text = "x"
-                        break
-                    case 13:
-                        // /
-                        totalLabel.text = "/"
-                        break
-                    case 14:
-                        // -
-                        totalLabel.text = "-"
-                        break
-                    case 15:
-                        // +
-                        totalLabel.text = "+"
-                        break
-                    case 16:
-                        // =
-                        performOperation()
-                        return
-                    case 17:
-                        // %
-                        totalLabel.text = "%"
-                        break
-                    default:
-                        print("Err in nested symbol touched switch")
-                        break
-                }
-                case 18:
-                // +/-
-                    currentNumber = toggleValue(currentNumber: currentNumber)
-                    return
-                default:
-                    totalLabel.text = ""
-                    print("Err in main symbol touched switch")
-                    break
+        if (Double(currentString)!.isNaN) {
+            symbol = title
+        } else {
+            print("current string: \(currentString)")
+            firstNumber = Double(currentString)!
+            isTypingNumber = false
         }
+        switch title {
+            case 12:
+                // *
+                totalLabel.text = "x"
+                break
+            case 13:
+                // /
+                totalLabel.text = "/"
+                break
+            case 14:
+                // -
+                totalLabel.text = "-"
+                break
+            case 15:
+                // +
+                totalLabel.text = "+"
+                break
+            case 17:
+                // %
+                totalLabel.text = "%"
+                break
+            default:
+                isFirstOperator = true
+                print("Err in symbol touched switch")
+                break
+        }
+
         symbol = title
     }
     
@@ -126,33 +107,36 @@ class ViewController: UIViewController {
      * This function will take the code sent from the button pressed by the user and perform the appropriate operation
      */
     func performOperation() {
+        var result: Double
         print("Operation symbol code: \(symbol)")
         switch symbol {
-        case 12:
-            firstNumber *= currentNumber
-            break
-        case 13:
-            firstNumber /= currentNumber
-            print("\(firstNumber) / \(currentNumber)")
-            break
-        case 14:
-            firstNumber -= currentNumber
-            break
-        case 15:
-            firstNumber += currentNumber
-            break
-        case 17:
-//            previousNumber = previousNumber % currentNumber
-            break
-        default:
-            print("Err performing operation")
+            case 12:
+                result = firstNumber * currentNumber
+                break
+            case 13:
+                result = firstNumber / currentNumber
+                print("\(firstNumber) / \(currentNumber)")
+                break
+            case 14:
+                result = firstNumber - currentNumber
+                break
+            case 15:
+                result = firstNumber + currentNumber
+                break
+            case 17:
+                result = 0
+//                result = firstNumber % currentNumber
+    //            previousNumber = previousNumber % currentNumber
+                break
+            default:
+                print("Err performing operation")
+                return
         }
         print("Operation prev num: \(firstNumber)")
-//        currentNumber = 0
-        totalLabel.text = String(firstNumber)
-        
+        totalLabel.text = String(result)
+        firstNumber = result
         isTypingNumber = true
-//        isFirstNumber = true
+        isFirstOperator = true
     }
     
     /**
